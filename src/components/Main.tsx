db2cb219cd2d4905896e15fd2f2510ade06c1afc
Branislav Jovanovic <job1bg@bosch.com>
2025-02-06 12:33:19 +0100

    Fix scrolling when multiple card elements appear.

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,7 +4,58 @@ export const Main = () => {
       <div className="board-header">
         <h2>Board</h2>
       </div>
-      <div className="board-main"></div>
+      <div className="board-main">
+        <div className="container-main">
+          <div className="card-main">
+            <div className="list-body">
+              <div className="list-item">
+                <span>To do</span>
+                <button className="list-item-button">...</button>
+              </div>
+            </div>
+          </div>
+          <div className="card-main">
+            <div className="list-body">
+              <div className="list-item">
+                <span>To do</span>
+                <button className="list-item-button">...</button>
+              </div>
+            </div>
+          </div>
+          <div className="card-main">
+            <div className="list-body">
+              <div className="list-item">
+                <span>To do</span>
+                <button className="list-item-button">...</button>
+              </div>
+            </div>
+          </div>
+          <div className="card-main">
+            <div className="list-body">
+              <div className="list-item">
+                <span>To do</span>
+                <button className="list-item-button">...</button>
+              </div>
+            </div>
+          </div>
+          <div className="card-main">
+            <div className="list-body">
+              <div className="list-item">
+                <span>To do</span>
+                <button className="list-item-button">...</button>
+              </div>
+            </div>
+          </div>
+          <div className="card-main">
+            <div className="list-body">
+              <div className="list-item">
+                <span>To do</span>
+                <button className="list-item-button">...</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
