0433fb35290d29b909e7931826829cb4cdc502e8
Branislav Jovanovic <job1bg@bosch.com>
2025-02-10 11:37:36 +0100

    Fix for SCSS imports.

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,9 @@ export default {
   preset: "ts-jest",
   testEnvironment: "jsdom",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  moduleNameMapper: {
+    "\\.(scss|sass|css)$": "<rootDir>/__mocks__/styleMock.js",
+  },
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest",
   },
